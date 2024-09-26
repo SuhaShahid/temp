9600e1f3b1aa8ff1dacbc671c1295b570213df0e
SuhaShahid <suhashahid.123@gmail.com>
2024-09-26 21:11:31 +0500

    apis

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const BaseController = require("./BaseController.js");
-const db = require("../models/index.js")
+const db = require("../models/index.js");
+const user = require("../models/user.js");
 
 class UserController extends BaseController {
   constructor() {
@@ -8,25 +9,35 @@ class UserController extends BaseController {
   }
 
   async getUsers(req, res) {
-    const users = await db.User.findAll()
-    console.log("test", users)
+    const users = await db.User.findAll();
+    console.log("test", users);
     res.status(200).json({
-        users: users,
-        message: "Users fetched successfully"
-    })
+      users: users,
+      message: "Users fetched successfully",
+    });
   }
 
   async createUser(req, res) {
-    console.log("user req", req.body)
+    console.log("user req", req.body);
 
-    const user = await db.User.create(req.body)
-    console.log("test", user)
+    const user = await db.User.create(req.body);
+    console.log("test", user);
     res.status(200).json({
-        user: user,
-        message: "Users created successfully"
-    })
+      user: user,
+      message: "Users created successfully",
+    });
   }
 
+  async getUserById(req, res) {
+    const user = await db.User.findOne({
+      where: { id: req.params.id },
+      rejectOnEmpty: true,
+    });
+    res.status(200).json({
+      user: user,
+      message: "User fetched successfully",
+    });
+  }
 }
 
 module.exports = new UserController();
--- router/user.route.js
+++ router/user.route.js
@@ -4,6 +4,7 @@ const UserController = require("../controllers/UserController.js");
 
 router.get("/", UserController.getUsers);
 router.post("/create-user", UserController.createUser);
+router.get("/get-user-id/:id", UserController.getUserById);
 
 
 module.exports = router;
